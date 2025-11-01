107058554a7e7e95fefd1033423ea5f08dccf585
masket <masket.siune@my.jcu.edu.au>
2025-11-01 19:55:17 +1000

    Modifications, final modification

--- used_cars.py
+++ used_cars.py
@@ -10,11 +10,13 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
     my_car.drive(30)
+    my_car.name = "my_car"
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
     limo = Car(120)
     limo.drive(115)
+    limo.name = "limo"
     print(f"Car has fuel: {limo.fuel}")
     print(limo)
 
