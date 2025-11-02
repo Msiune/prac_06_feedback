6ac3492ce8fb43cb08e1ed5e5c9a8ac404406b03
masket <masket.siune@my.jcu.edu.au>
2025-11-02 21:18:15 +1000

    Intermediate Exercises, programming_language

---
+++ languages.py
@@ -0,0 +1,14 @@
+"""Programming Language"""
+
+"""
+Estimated Time: 30minute
+Start time:8:00
+End time:8:50
+"""
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
---
+++ programming_language.py
@@ -0,0 +1,32 @@
+"""Programming Language"""
+
+"""
+Estimated Time: 30minute
+Start time:8:00
+End time:8:50
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name = "", typing = "Static",reflection =False, year = 0):
+
+        self.name = name
+        self.year = year
+        self.typing = typing
+        self.reflection = reflection
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+    def has_reflection(self):
+            return self.reflection == True
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} typing, reflection = {self.reflection}, First appeared in {self.year}"
+
+
+
+
+
+
+
